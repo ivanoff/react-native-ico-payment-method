2a8440538143fd273cc7a3f12a1052cab4f628ef
Dimitry Ivanov <2@ivanoff.org.ua>
2023-09-09 22:45:33 +0300

    add types

--- typings.d.ts
+++ typings.d.ts
@@ -49,10 +49,35 @@ declare module 'react-native-ico-payment-method' {
       'amazon' |
       '2co';
 
+    type backgroundType = 'circle' | 'rect' | 'button';
+
+    type positionType = 'top' | 'bottom' | 'right' | 'left' |
+      'top_right' | 'top_left' | 'bottom_right' | 'bottom_left';
+
+    type backgroundTypeObj = {
+      type?: backgroundType;
+      color?: string;
+      radius?: number;
+      borderRadius?: number;
+    };
+
+    type badgeTypeObj = {
+      value?: number | string;
+      fontSize?: string;
+      position?: positionType;
+      color?: string;
+      backgroundColor?: string;
+      radius?: number;
+    };
+
     interface IconProps {
       name: iconNames;
-      color: string;
-      size: number;
+      width?: number;
+      height?: number;
+      color?: string;
+      stroke?: number;
+      background?: backgroundType | backgroundTypeObj;
+      badge?: number | string | badgeTypeObj;
     }
 
     const Icon: (props: IconProps) => ReactNode;
@@ -60,4 +85,3 @@ declare module 'react-native-ico-payment-method' {
     export { iconNames };
     export default Icon;
 }
-
